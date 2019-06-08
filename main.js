a634d6d6862c81737be67e97b8bf6adc8585247b
shizzarap112005 <51516726+shizzarap112005@users.noreply.github.com>
2019-06-08 15:58:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
   $('.header').height($(window).height());
 })
-.header {
-  background-image: url('images/header-background.jpg');
-  background-size: cover;
-  background-position: center;
-  position: relative;
-}
