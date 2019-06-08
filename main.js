af54688789ca8c31bc7b24f9583ec5e8cfd33d79
shizzarap112005 <51516726+shizzarap112005@users.noreply.github.com>
2019-06-08 15:57:29 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.header').height($(window).height());
+})
+.header {
+  background-image: url('images/header-background.jpg');
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
